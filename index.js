3df3efef033c517de22e6bb8ee935dab4e2f2dac
alpaka206 <gyuwon05@gmail.com>
2024-07-17 17:52:26 +0900

    vercel 배포 성공

--- index.js
+++ index.js
@@ -4,29 +4,20 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./db");
 const userRoutes = require("./routes/users");
 const frameRoutes = require("./routes/frame");
-// const path = require("path");
-// const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 connectDB();
-
-// // 'uploads' 폴더가 존재하지 않으면 생성
-// const uploadsDir = path.join(__dirname, "uploads");
-// if (!fs.existsSync(uploadsDir)) {
-//   fs.mkdirSync(uploadsDir);
-// }
 
 app.use("/api/users", userRoutes);
 app.use("/api/frame", frameRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hesllo World");
 });
 
 module.exports = app;
